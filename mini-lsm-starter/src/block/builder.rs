b1b66796c52c80d1a23f018cbae58837878c7f2f
wjc <329512443@qq.com>
2025-03-09 21:22:39 +0800

    w1d3

--- mini-lsm-starter/src/block/builder.rs
+++ mini-lsm-starter/src/block/builder.rs
@@ -12,12 +12,11 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#![allow(unused_variables)] // TODO(you): remove this lint after implementing this mod
-#![allow(dead_code)] // TODO(you): remove this lint after implementing this mod
+use bytes::BufMut;
 
 use crate::key::{KeySlice, KeyVec};
 
-use super::Block;
+use super::{Block, U16_SIZE};
 
 /// Builds a block.
 pub struct BlockBuilder {
@@ -31,25 +30,75 @@ pub struct BlockBuilder {
     first_key: KeyVec,
 }
 
+fn compute_overlap(s1: &KeySlice, s2: &KeySlice) -> usize {
+    let mut ret = 0;
+    loop {
+        if ret >= s1.len() || ret >= s2.len() {
+            break;
+        }
+        if s1.raw_ref()[ret] != s2.raw_ref()[ret] {
+            break;
+        }
+        ret += 1;
+    }
+    ret
+}
+
 impl BlockBuilder {
     /// Creates a new block builder.
     pub fn new(block_size: usize) -> Self {
-        unimplemented!()
+        Self {
+            offsets: Vec::new(),
+            data: Vec::new(),
+            block_size,
+            first_key: KeyVec::new(),
+        }
+    }
+
+    fn estimated_size(&self) -> usize {
+        U16_SIZE + self.offsets.len() * U16_SIZE + self.data.len()
     }
 
     /// Adds a key-value pair to the block. Returns false when the block is full.
     #[must_use]
     pub fn add(&mut self, key: KeySlice, value: &[u8]) -> bool {
-        unimplemented!()
+        assert!(!key.is_empty(), "key must not be empty");
+        if !self.is_empty()
+            && self.estimated_size() + key.len() + value.len() + 3 * U16_SIZE > self.block_size
+        {
+            return false;
+        }
+
+        // | overlap | stored key len | key data | value len | value data |
+        self.offsets.push(self.data.len() as u16);
+        let overlap = compute_overlap(&self.first_key.as_key_slice(), &key);
+        self.data.put_u16(overlap as u16);
+        self.data.put_u16((key.len() - overlap) as u16);
+        let pre_trim_key = &key.raw_ref()[overlap..];
+        self.data.put(pre_trim_key);
+        self.data.put_u16(value.len() as u16);
+        self.data.put(value);
+
+        if self.first_key.is_empty() {
+            self.first_key = key.to_key_vec();
+        }
+
+        true
     }
 
     /// Check if there is no key-value pair in the block.
     pub fn is_empty(&self) -> bool {
-        unimplemented!()
+        self.offsets.is_empty()
     }
 
     /// Finalize the block.
     pub fn build(self) -> Block {
-        unimplemented!()
+        if self.is_empty() {
+            panic!("block should not be empty");
+        }
+        Block {
+            data: self.data,
+            offsets: self.offsets,
+        }
     }
 }
--- mini-lsm-starter/src/block/iterator.rs
+++ mini-lsm-starter/src/block/iterator.rs
@@ -12,12 +12,14 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#![allow(unused_variables)] // TODO(you): remove this lint after implementing this mod
-#![allow(dead_code)] // TODO(you): remove this lint after implementing this mod
-
 use std::sync::Arc;
 
-use crate::key::{KeySlice, KeyVec};
+use bytes::Buf;
+
+use crate::{
+    block::U16_SIZE,
+    key::{KeySlice, KeyVec},
+};
 
 use super::Block;
 
@@ -35,57 +37,111 @@ pub struct BlockIterator {
     first_key: KeyVec,
 }
 
+impl Block {
+    fn get_first_key(&self) -> KeyVec {
+        let mut entry = self.data.as_slice();
+        entry.get_u16();
+        let key_size = entry.get_u16() as usize;
+        KeyVec::from_vec(entry[..key_size].to_vec())
+    }
+}
+
 impl BlockIterator {
     fn new(block: Arc<Block>) -> Self {
         Self {
+            first_key: block.get_first_key(),
             block,
             key: KeyVec::new(),
             value_range: (0, 0),
             idx: 0,
-            first_key: KeyVec::new(),
         }
     }
 
     /// Creates a block iterator and seek to the first entry.
     pub fn create_and_seek_to_first(block: Arc<Block>) -> Self {
-        unimplemented!()
+        let mut iter = Self::new(block);
+        iter.seek_to_first();
+        iter
     }
 
     /// Creates a block iterator and seek to the first key that >= `key`.
     pub fn create_and_seek_to_key(block: Arc<Block>, key: KeySlice) -> Self {
-        unimplemented!()
+        let mut iter = Self::new(block);
+        iter.seek_to_key(key);
+        iter
     }
 
     /// Returns the key of the current entry.
     pub fn key(&self) -> KeySlice {
-        unimplemented!()
+        debug_assert!(self.is_valid(), "invalid block iterator");
+        self.key.as_key_slice()
     }
 
     /// Returns the value of the current entry.
     pub fn value(&self) -> &[u8] {
-        unimplemented!()
+        debug_assert!(self.is_valid(), "invalid block iterator");
+        &self.block.data[self.value_range.0..self.value_range.1]
     }
 
     /// Returns true if the iterator is valid.
     /// Note: You may want to make use of `key`
     pub fn is_valid(&self) -> bool {
-        unimplemented!()
+        !self.key.is_empty()
+    }
+
+    pub fn seek_to_idx(&mut self, idx: usize) {
+        if self.block.offsets.len() <= idx {
+            self.key.clear();
+            self.value_range = (0, 0);
+            self.idx = idx;
+            return;
+        }
+        let offset_set = self.block.offsets[idx] as usize;
+        let mut entry = &self.block.data[offset_set..];
+        // key
+        self.key.clear();
+        let overlap_len = entry.get_u16() as usize;
+        let overlap_part = &self.first_key.raw_ref()[..overlap_len];
+        self.key.append(overlap_part);
+        let key_len = entry.get_u16() as usize;
+        let key_part = &entry[..key_len];
+        self.key.append(key_part);
+        entry.advance(key_len);
+        // value_range
+        let value_len = entry.get_u16() as usize;
+        let value_start = offset_set + U16_SIZE * 3 + key_len;
+        let value_end = value_start + value_len;
+        self.value_range = (value_start, value_end);
+        // idx
+        self.idx = idx;
     }
 
     /// Seeks to the first key in the block.
     pub fn seek_to_first(&mut self) {
-        unimplemented!()
+        self.seek_to_idx(0);
     }
 
     /// Move to the next key in the block.
     pub fn next(&mut self) {
-        unimplemented!()
+        self.seek_to_idx(self.idx + 1);
     }
 
     /// Seek to the first key that >= `key`.
     /// Note: You should assume the key-value pairs in the block are sorted when being added by
     /// callers.
     pub fn seek_to_key(&mut self, key: KeySlice) {
-        unimplemented!()
+        let mut left = 0;
+        let mut right = self.block.offsets.len();
+        while left != right {
+            let mid = left + (right - left) / 2;
+            self.seek_to_idx(mid);
+            assert!(self.is_valid());
+            match self.key().cmp(&key) {
+                std::cmp::Ordering::Equal => return,
+                std::cmp::Ordering::Greater => right = mid,
+                std::cmp::Ordering::Less => left = mid + 1,
+            }
+        }
+        self.seek_to_idx(right);
     }
 }
